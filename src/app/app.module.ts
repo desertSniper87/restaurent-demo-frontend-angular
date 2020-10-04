346ff910406bdd7e6a62095e39b78d1cbd2707f0
desertsniper87 <torshobuet@gmail.com>
2020-10-04 15:50:54 +0600

    router implementation init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,15 @@ import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from "./services/dish.service";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-// import {  } from "";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from "./services/dish.service";
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
@@ -26,6 +31,9 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,7 +45,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService

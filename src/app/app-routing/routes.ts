edc2532dc60bd7f5bff66019ebf4592263711b9a
desertsniper87 <torshobuet@gmail.com>
2020-10-05 12:48:25 +0600

    dishdetail in router

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from "../home/home.component";
 import { AboutComponent } from "../about/about.component";
 import { MenuComponent } from "../menu/menu.component";
 import { ContactComponent } from "../contact/contact.component";
+import { DishdetailComponent } from "../dishdetail/dishdetail.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -10,4 +11,5 @@ export const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'menu', component: MenuComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'dishdetail/:id', component: DishdetailComponent},
 ]
